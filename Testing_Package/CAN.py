aa6c87ab7d8d0d82a70d0bc0a5bbc1c35bf9bec2
DavidMe03 <vidamed@MacBook-Pro23>
2024-10-20 14:58:09 -0400

    Deleted files from last year that are not useful now

--- Testing_Package/CAN.py
+++
@@ -1,5 +0,0 @@
-def sendCANmessage(message: str) -> None:
-    pass
-
-def getCANmessages() -> list[str]:
-    pass
--- Testing_Package/Exceptions.py
+++
@@ -1,7 +0,0 @@
-class STlinkException(Exception):
-    def __init__(self, *args: object) -> None:
-        super().__init__(*args)
-
-class InvalidInputException(Exception):
-    def __init__(self, *args: object) -> None:
-        super().__init__(*args)
