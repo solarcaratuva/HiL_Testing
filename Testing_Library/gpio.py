9dde66875f79a3d889f73f9667a946f81941659b
Andrew Amos <amosandrewj@gmail.com>
2024-11-07 17:35:14 -0500

    Structure of Classes

--- Testing_Library/gpio.py
+++ Testing_Library/gpio.py
@@ -1,1 +1,16 @@
-"hello"
+import gipozero
+
+"""
+DigitalOutput class
+    Parameters:
+        Pinname : String (same style as pinmap.h; validate pin name)
+    Methods:
+        Write (validate input based on established range)
+        
+
+DigitalInput class
+    Parameters:
+        Pinname : String (same style as pinmap.h; validate pin name)
+    Methods:
+        Read : bool
+"""
