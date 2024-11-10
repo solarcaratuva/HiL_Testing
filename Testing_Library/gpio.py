927c198f8476a570de2946a78239a0bd30b6c7bf
DavidMe03 <vidamed@MacBook-Pro23>
2024-11-10 15:07:39 -0500

    Simplified __PinValidate() function

--- Testing_Library/gpio.py
+++ Testing_Library/gpio.py
@@ -18,18 +18,13 @@ class DigitalOutput:
 
     #Validate the Pin Number
     #INPUT: self and pinName as a string
-    #OUTPUT: Sets 
+    #OUTPUT: returns the pin_number from mapping
     def __PinValidate(self, pinName):
         #pin mapping json
         config_file = "config.json"
         # Load pin mappings from the config file
-        try:
-            with open(config_file, 'r') as f:
-                pin_map = json.load(f)
-        except FileNotFoundError:
-            raise FileNotFoundError(f"Config file '{config_file}' not found.")
-        except json.JSONDecodeError:
-            raise ValueError(f"Config file '{config_file}' contains invalid JSON.")
+        with open(config_file, 'r') as f:
+            pin_map = json.load(f)
         
         # Map the pin name to an integer GPIO pin number
         if pinName in pin_map:
@@ -81,18 +76,21 @@ DigitalInput class
 class DigitalInput:
     #Validate the Pin Number
     #INPUT: self and pinName as a string
-    #OUTPUT: Sets 
+    #OUTPUT: returns the pin_number from mapping
     def __PinValidate(self, pinName):
         #pin mapping json
         config_file = "config.json"
         # Load pin mappings from the config file
-        try:
-            with open(config_file, 'r') as f:
-                pin_map = json.load(f)
-        except FileNotFoundError:
-            raise FileNotFoundError(f"Config file '{config_file}' not found.")
-        except json.JSONDecodeError:
-            raise ValueError(f"Config file '{config_file}' contains invalid JSON.")
+        with open(config_file, 'r') as f:
+            pin_map = json.load(f)
+        
+        # Map the pin name to an integer GPIO pin number
+        if pinName in pin_map:
+            pin_number = pin_map[pinName]
+        else:
+            raise ValueError(f"Pin name '{pinName}' not found in config file.")
+        
+        return pin_number
 
     #Constructor
     def __init__(self, pinName: str):
