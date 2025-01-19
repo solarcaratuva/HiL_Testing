8e7bb21d76beb1e591da5a204b149581accf54db
pizzeman <amosandrewj@gmail.com>
2025-01-19 15:11:05 -0500

    Added Comments

--- Testing_Library/CANPi.py
+++ Testing_Library/CANPi.py
@@ -22,29 +22,31 @@ def readIn():
         mbed_serial.reset_input_buffer()
         response = mbed_serial.readline()
         print(f'READ ENCODED MESSAGE: {response}\n')
-        return decodeCANMessage(response)
+        #Convert message into CAN Format using CANMessage
+        id_data = int.from_bytes(response[0:2], "big")
+        CANData = CANMessage.decode_message(id_data, response[2:], int(time.time()))
+        return CANData
         
     except serial.SerialException:
         print("Serial Exception Error")
 
     except serial.SerialTimeoutException:
-        print("Serial Timeout Error")
-
-def decodeCANMessage(response):
-    #Convert message into CAN Format using CANMessage
-    id_data = int.from_bytes(response[0:2], "big")
-    CANData = CANMessage.decode_message(id_data, response[2:], int(time.time()))
-    return CANData
+        print("Serial Timeout Error")    
         
 #'CAN' Write Using PySerial
 def writeOut(tx_data):
     print(f'WRITING TO PIN: {tx_data}') 
-    mbed_serial.write(tx_data)
+    mbed_serial.write(tx_data.encode_message())
+
+
+
+
+
 
 #Use this to test encoding (raspberry pi to nucleo)
 def defaultCANEncodeTest():
     testCAN = CANMessage.CanMessage("MotorCommands", 0x200, {}, 0)
-    writeOut(testCAN.encode_message())
+    writeOut(testCAN)
     data = readIn()
     print(data)
 
