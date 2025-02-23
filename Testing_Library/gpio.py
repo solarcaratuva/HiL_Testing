40ddaf63c8272098449e814fc698941e2379262c
DavidMe03 <davidmerino407@gmail.com>
2025-02-23 14:56:51 -0500

    Added AnalogOut class to gpio.py and gpioPins.py

--- Testing_Library/gpio.py
+++ Testing_Library/gpio.py
@@ -119,3 +119,61 @@ class DigitalInput:
     def read(self) -> bool:
         return self.pinObject.is_active
 
+
+"""
+AnalogOutput class
+    Parameters:
+        Pinname : String (same style as pinmap.h; validate pin name)
+    Methods:
+        Write (validate input based on established range)
+"""
+
+class AnalogOutput:
+    #Validate the Pin Number
+    #INPUT: self and pinName as a string
+    #OUTPUT: returns the pin_number from mapping
+    def __PinValidate(self, pinName: str) -> bool:
+        main_cpp_dict = parse_gpio_pins()
+        pin_def_dict = parse_nucleo_pindef_pins()
+
+        #pin mapping json
+        config_file = os.path.join(os.path.dirname(__file__), "config.json")
+        # Load pin mappings from the config file
+        with open(config_file, 'r') as f:
+            pin_mappings = json.load(f)
+        
+        # Map the pin name to an integer pin number
+        if main_cpp_dict[pinName] == "DigitalOut":
+            nucleo_pin_number = pin_mappings["PowerBoard_to_nucleo_pin_mapping"][pin_def_dict[pinName]]
+            pi_pin_number = pin_mappings["nucleo_to_pi_mapping"][f"{nucleo_pin_number}"]
+        else:
+            raise ValueError(f"Pin name '{pinName}' not found.")
+
+        return pi_pin_number
+    
+    #Constructor
+        #Active-High
+        #Default Duty Cycle: 0
+        #Default freq: 100 Hz ;)
+    def __init__(self, pinName: str):
+        self.pinName = pinName
+        self.pinNumber = self.__PinValidate(pinName=pinName)
+        self.pinObject = gpiozero.PWMOutputDevice(self.pinNumber)
+
+    #Accepts values of [0,1]
+    def set_duty_cycle(self, value: float):
+        if 0.0 <= value <= 1.0:
+            self.pinObject.value = value
+        
+        #else: do nothing
+        
+    #sets duty cycle to 1.0
+    def on(self) -> None:
+        self.pinObject.on()
+
+    #sets duty cycle to 0.0
+    def off(self) -> None:
+        self.pinObject.off()
+
+
+
