21ff3a052e5979a9cfda3246714fbd65217d55d1
colbycheese55 <112818133+colbycheese55@users.noreply.github.com>
2025-03-03 22:35:41 -0500

    deleted FakeRivanna2

--- FakeRivanna2/PowerBoard/tests/PbExampleTest2.py
+++
@@ -1,7 +0,0 @@
-import unittest
-
-class ExampleTestStrings1(unittest.TestCase):
-
-    def test_string_split(self):
-        """ Test splitting strings within power board """
-        self.assertEqual("hixhi".split("x"), ["hi", "hi"])
