dcff2cac8435abc9f712a08ef84972f0b572fcc9
colbycheese55 <112818133+colbycheese55@users.noreply.github.com>
2025-03-22 13:21:56 -0400

    input validation for read_in_configs()

--- Server/config.py
+++ Server/config.py
@@ -13,19 +13,23 @@ PIN_TYPES_MAP = dict()
 LOG_FOLDER = os.path.join(os.path.dirname(__file__), "..", "logs")
 
 
-def read_in_configs(repoName: str) -> None:
-    """Reads in server and repo config files for a given repo. Sets global constants."""
+def read_in_configs(repoName: str) -> bool:
+    """Reads in server and repo config files for a given repo. Sets global constants. Returns True if `repoName` is a valid repo, False otherwise."""
     global SERVER_CONFIG, REPO_CONFIG, REPO_ROOT
 
     server_congig_path = os.path.join(os.path.dirname(__file__), "server_config.json")
     with open(server_congig_path, 'r') as file:
         SERVER_CONFIG = json.load(file)
 
+    if repoName not in SERVER_CONFIG["repo_paths"]:
+        return False
     REPO_ROOT = SERVER_CONFIG["repo_paths"][repoName]
     
     repo_config_path = os.path.join(REPO_ROOT, "hil_config.json")
     with open(repo_config_path, 'r') as file:
         REPO_CONFIG = json.load(file)
+
+    return True
 
 def read_in_nucleo_pinmaps(board: str) -> None:
     """Parses pin data for a given board in a given repo. Sets global constants. Must have called `read_in_configs()` already."""
