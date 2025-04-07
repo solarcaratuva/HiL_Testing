9d44c8b664191c05f2db9d3d5692d094a5ea7d08
Colby Wise <cwise1080@gmail.com>
2025-04-06 20:11:06 -0400

    Fixed Some Syntax Errors

---
+++ Testing_Library/test_can_bus.py
@@ -0,0 +1,32 @@
+from CANBus import CANBus
+from CANMessage import CanMessage
+import time
+
+def testAddMethod(canBus : CANBus):
+	canBus.printCANBus()
+
+	input("WAITING")
+	#Example CANMessage
+	name = "BPSError"
+	id = 0x106
+	signals = {"internal_communications_fault" : 1}
+	timestamp = 1.0
+	can_message = CanMessage(name, id, signals, timestamp)
+
+
+	canBus.addToCANBus(can_message)
+	canBus.printCANBus()
+
+
+canBus = CANBus()
+#testAddMethod(canBus)
+
+counter = 0
+while (counter < 10):
+	print(f'Reading number {counter}')
+	canBus.printCANBus()
+	time.sleep(0.5)
+	counter = counter + 1
+
+
+canBus.stopReadThread()
