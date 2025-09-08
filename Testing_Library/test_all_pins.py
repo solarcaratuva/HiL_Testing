3c20f445e7d5866f7aa3098c48420a0e8f9db410
Colby Wise <cwise1080@gmail.com>
2025-09-07 20:09:09 -0400

    Added Test for GPIO Input and Output in test_all_pins

--- Testing_Library/test_all_pins.py
+++ Testing_Library/test_all_pins.py
@@ -14,7 +14,18 @@ def turnAllOn():
 		pinOut.on()
 		input("WAITING TO TURN ON: " + str(i+1))
 
-turnAllOn()
-turnAllOff()
-time.sleep(3)
-print("Ending")
+def testGPIOInAndOut():
+	#Test GPIO2 (output) and GPIO11 (input)
+	#Turn GPIO2 on (default state) and GPIO11 should turn on from the nucleo; can also use wait_for_active
+	
+	#turn on GPIO2
+	pinOut = gpiozero.DigitalOutputDevice("GPIO2");
+	pinIn = gpiozero.DigitalInputDevice("GPIO11");
+
+	pinOut.on()
+	while True:
+		print(f'THIS IS THE PIN IN VALUE (GPIO 11): {pinIn.value}')
+		print(f'THIS IS THE PIN OUT VALUE (GPIO 2): {pinOut.value}')
+		#pinIn.wait_for_active()
+		#print("IT IS ACTIVE")
+		time.sleep(3)
