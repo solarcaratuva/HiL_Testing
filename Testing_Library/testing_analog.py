03925fe2db06b318d0c4b567137d62547e2fdbd5
Colby Wise <cwise1080@gmail.com>
2025-09-08 05:51:37 -0400

    Finished the AnalogOut Pins Class

--- Testing_Library/testing_analog.py
+++ Testing_Library/testing_analog.py
@@ -1,12 +1,39 @@
 import gpiozero
 import time
+from gpioPins import AnalogOutput
 
-pinOut = gpiozero.PWMOutputDevice("GPIO5", frequency=5000)
 
-#Sending analog value to nucleo
-while True:
-	pinOut.value = 0.64
-	print(f"SENDING {pinOut.value}")
-    #pinOut.pulse() #Useful function to sweep all ranges
+pinOut = AnalogOutput("5")
 
-	time.sleep(2)
+#Test case without the wrapper api
+def send_to_nucleo():	
+	pinOut = gpiozero.PWMOutputDevice("GPIO5", frequency=5000)
+
+	#Sending analog value to nucleo
+	while True:
+		pinOut.value = 0.64
+		print(f"SENDING {pinOut.value}")
+		#pinOut.pulse() #Useful function to sweep all ranges
+
+		time.sleep(2)
+
+#Test the wrapper api
+def test_analog_class():
+	counter = 0.0
+	print("TESTING VOLTAGE WRITE")
+	while counter < 3.3:
+		pinOut.write_voltage(counter)
+		print(f'READING IN PIN VALUE: {pinOut.read()}')
+		time.sleep(2)
+		counter = counter + 0.3
+	
+	counter = 0.0
+	print("TESTING PERCENTAGE WRITE")
+	while counter < 1:
+		pinOut.write(counter)
+		print(f'READING IN PIN VALUE: {pinOut.read()}')
+		time.sleep(2)
+		counter = counter + 0.1
+		
+
+test_analog_class()
