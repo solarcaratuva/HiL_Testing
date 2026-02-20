8b900820b005f31738fcbb0a837e8acbb51897cc
Abhinav Pappu <53156924+abhinavp5@users.noreply.github.com>
2026-02-20 18:25:07 -0500

    Test motor interface (#21)
    * Test script for recieving arudino i2c data serially with pi
    
    * debuggin output to motor_interface.py
    
    * 3 bytes read for motorinterface.py
    
    * Working serial communication with pi and arduino
    
    * implementation of MotorInterfaceClass
    
    * fixes to self.lock
    
    * confirmed working implemntatin of MotorInterface APi
    
    * updates to motor interface API
    
    * remove unused instance of MotorInterfaceTest and related test loop
    
    * Adding serial port as member of class
    
    * fix: debug output
    
    * test: added the arduino testscript
    
    * added arudino script
    
    * New File for System Wide Testing
    
    * Developing Testing for all Comms
    
    * Added Test for Digital, Analog and CAN
    
    * Changing Tabs
    
    * fix: exception when stop reading for less error-y exit from test
    
    * feat: add motor interface test script for throttle and regen modes
    
    * Changed Syntax in Test_All_Comms
    
    * fix: added locks, and updated exceptions
    
    * fix: deleted dupe ard script#
    
    * fix: capitalized lock
    
    * fix: fixed deadlock issue
    
    * fix: added close method to closer serial port
    
    * fix: reverted TestRunner
    
    * fix: final changes to review
    
    ---------
    
    Co-authored-by: Colby Wise <cwise1080@gmail.com>
    Co-authored-by: Andrew Amos <amosandrewj@gmail.com>

---
+++ Testing_Library/test_all_comms.py
@@ -0,0 +1,106 @@
+import sys
+sys.path.append('/home/solarcar/solarcar/HiL_Testing/Server')
+
+from CANBus import CANBus
+from CANMessage import CanMessage
+from CANPi import readIn
+import time
+import serial
+from gpioPins import DigitalInput, DigitalOutput
+import random
+import gpiozero
+from gpioPins import AnalogOutput
+
+# Global Pins for Synchronization
+pinOut = DigitalOutput("2")
+pinIn = DigitalInput("11")
+
+# -----------Testing CAN Comms-----------
+
+def testAddMethod(canBus : CANBus):
+	canBus.printCANBus()
+
+	input("WAITING")
+	#Example CANMessage
+	name = "BPSError"
+	id = 0x106
+	signals = {"internal_communications_fault" : 1}
+	timestamp = 1.0
+	can_message = CanMessage(name, id, signals, timestamp)
+	canBus.addToCANBus(can_message)
+	canBus.printCANBus()
+
+def testReadThread(canBus : CANBus):
+	time.sleep(1) # Wait for Nucleo to send CAN
+	counter = 0
+	while (counter < 5):
+		print(f'Reading number {counter}')
+		canBus.printCANBus()
+		time.sleep(0.5)
+		counter = counter + 1
+		
+#Send BPSError to Nucleo (currently turn LD2 off and on)
+def testWrite(canBus : CANBus):
+	#Example CANMessage
+	name = "BPSError"
+	id = 0x106
+	signals = {"internal_communications_fault" : 1}
+	timestamp = 1.0
+	can_message = CanMessage(name, id, signals, timestamp)
+	counter = 0
+	canBus.sendMessage(can_message)
+		
+def testCAN():
+	canBus = CANBus()
+	testWrite(canBus)
+
+	pinOut.write(0) # Block nucleo from proceeding
+	# Check that nucleo modified pin to 1 to switch to receiving
+	while pinIn.read() == 0:
+		pinOut.write(0)
+		time.sleep(0.5)
+
+	testReadThread(canBus)
+
+#----------------Testing Analog Comms---------------
+
+#Test case without the wrapper api
+def send_to_nucleo():	
+	pinOut = gpiozero.PWMOutputDevice("GPIO5", frequency=5000)
+
+	#Sending analog value to nucleo
+	while True:
+		pinOut.value = 0.64
+		print(f"SENDING {pinOut.value}")
+		#pinOut.pulse() #Useful function to sweep all ranges
+
+		time.sleep(2)
+
+#Test the wrapper api
+def test_analog_class(pinOut):
+	value = round(random.uniform(1.0, 100.0), 2) # Generate random 2 d.p. float
+	
+	pinOut.write_voltage(value)
+	print(f'READING IN PIN VALUE: {pinOut.read()}')
+
+def testAnalog():
+	pinOut = AnalogOutput("5")
+	test_analog_class(pinOut)
+
+if __name__ == "__main__":
+	# Wait for nucleo
+	while pinIn.read() == 1:
+		pinOut.write(0)
+		time.sleep(0.5)
+
+	pinOut.write(1)
+	testCAN()
+
+	# Make sure finished sending
+	while pinIn.read() == 1:
+		pinOut.write(0)
+		print("THERE WAS A SYNC ERROR WITH RECEIVING CAN")
+		time.sleep(0.5)
+
+	pinOut.write(1)
+	testAnalog()
